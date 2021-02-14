54d706df386c884d952312d29a04339a535685d2
qa3aq <dimdac@gmail.com>
2021-02-14 17:30:41 +0500

    Finish user edit, update, index, and destroy actions

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
-  # test 'full title helper' do
-  #   assert_equal full_title,         full_title
-  #   assert_equal full_title('Help'), full_title('Help')
-  #   assert_equal full_title('About'), full_title('About')
-  #   assert_equal full_title('Contact'), full_title('Contact')
-  # end
+  test 'full title helper' do
+    assert_equal full_title,         full_title
+    assert_equal full_title('Help'), full_title('Help')
+    assert_equal full_title('About'), full_title('About')
+    assert_equal full_title('Contact'), full_title('Contact')
+  end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  
-  test 'layout links' do
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'layout links non-logged-in' do
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
@@ -10,7 +14,22 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
     assert_select 'a[href=?]', signup_path
-    # get contact_path
-    # assert_select 'title', full_title('Contact')
-    end
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', 'https://rubyonrails.org/'
+    assert_select 'a[href=?]', 'https://www.railstutorial.org/', count: 2
+    assert_select 'a[href=?]', 'https://www.michaelhartl.com/'
+    assert_select 'a[href=?]', 'https://news.railstutorial.org/', text: 'News'
+    get contact_path
+    assert_select 'title', full_title('Contact')
+  end
+
+  test 'layout links logged-in' do
+    log_in_as(@user)
+    get root_path
+    assert_select 'a[href=?]', users_path, text: 'Users'
+    assert_select 'a[href=?]', user_path(@user), text: 'Profile'
+    assert_select 'a[href=?]', edit_user_path(@user), text: 'Settings'
+    assert_select 'a[href=?]', logout_path, text: 'Log out'
+  end
+
 end
